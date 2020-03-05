1fb9d6cf23f132fceaa8f1252b83ee9f91c4e5ff
Cronino <tomantic@gmail.com>
2020-03-05 17:26:04 +0000

    pycharm setup

---
+++ plotly_test.py
@@ -0,0 +1,9 @@
+
+import plotly.express as px
+import pandas
+df = px.data.gapminder().query("year==2007")
+fig = px.scatter_geo(df, locations="iso_alpha", color="continent",
+                     hover_name="country", size="pop",
+                     projection="natural earth")
+
+fig.show()
